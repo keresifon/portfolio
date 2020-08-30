05d5b49476335c0cd11ed7e12c5f80271394b3f5
Keresifon <keresifon@gmail.com>
2020-08-30 18:26:36 +0100

    Update About.js

--- src/component/About.js
+++ src/component/About.js
@@ -18,7 +18,7 @@ function About(props) {
 <Accordion defaultActiveKey="0">
   <Card>
     <Accordion.Toggle as={Card.Header} eventKey="0" >
-    Work Experience
+    => Work Experience
     </Accordion.Toggle>
     <Accordion.Collapse eventKey="0">
       <Card.Body><Experience /></Card.Body>
@@ -26,7 +26,7 @@ function About(props) {
   </Card>
   <Card>
     <Accordion.Toggle as={Card.Header} eventKey="1">
-      Skills
+    => Skills
     </Accordion.Toggle>
     <Accordion.Collapse eventKey="1">
       <Card.Body><Skills/></Card.Body>
