8f419850d36ac12e889a1a6181e9dc41057f6a2f
Keresifon <keresifon@gmail.com>
2020-09-18 17:18:40 +0100

    animation

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {  Alert } from 'react-bootstrap';
+import { Alert } from 'react-bootstrap';
 import ReactTextRotator from 'react-text-rotator';
 import { FaFacebook, FaTwitter, FaGithub, FaInstagram } from 'react-icons/fa';
 import Content from '../component/Content';
+import { Animated } from 'react-animated-css';
 
 function Home(props) {
 	const content = [
@@ -28,7 +29,6 @@ function Home(props) {
 		},
 	];
 	return (
-		
 		<div className="container-fluid bg">
 			<div className="row">
 				<div className="col py-5">
@@ -67,57 +67,77 @@ function Home(props) {
 							<FaInstagram className="h1  iconColor" />
 						</a>
 					</div>
-					<div className=" ml-5 py-5">
-						<div className="welcome">Welcome</div>
-						<div className="h1  introColor">Hi, I'm Kere Ekpenyong</div>
-						<div>
-							<ReactTextRotator content={content} time={5000} startDelay={2000} />
+					<Animated animationIn="fadeIn" animationOut="fadeOut" animationInDuration={3000} isVisible={true}>
+						<div className=" ml-5 py-5">
+							<div className="welcome">Welcome</div>
+							<div className="h1  introColor">Hi, I'm Kere Ekpenyong</div>
+
+							<div>
+								<ReactTextRotator content={content} time={5000} startDelay={2000} />
+							</div>
 						</div>
-					</div>
+					</Animated>
 					<div className="pbl-6"></div>
-					<div>
-						<Alert variant="outline-danger" className="alert border">
-							<Alert.Heading>Credit & Dependencies</Alert.Heading>
-							<p>Ella , the model in the background, is one of my twin toddlers.</p>
-							<hr />
-							<p className="mb-0"></p>
-						
-						<div className="row">
-							<div className="col">
-								<div>
-									<ul>
-										<li>react</li>
-										<li>react-bootstrap</li>
-										<li>bootstrap</li>
-									</ul>
+					<Animated
+						animationIn="bounceInLeft"
+						animationOut="fadeOut"
+						animationInDuration={3000}
+						animationInDelay={1000}
+						isVisible={true}
+					>
+						<div id="dependencies">
+							<Alert variant="outline-danger" className="alert border">
+								<Alert.Heading>Credit & Dependencies</Alert.Heading>
+								<p>Ella , the model in the background, is one of my twin toddlers.</p>
+								<hr />
+								<p className="mb-0"></p>
+
+								<div className="row">
+									<div className="col">
+										<div>
+											<ul>
+												<li>react</li>
+												<li>react-bootstrap</li>
+												<li>bootstrap</li>
+												<li>react-animated-css</li>
+											</ul>
+										</div>
+									</div>
+									<div className="col">
+										<div>
+											<ul>
+												<li>react-icons</li>
+												<li>react-text-rotator</li>
+												<li>react-icons</li>
+											</ul>
+										</div>
+									</div>
+									<div className="col">
+										<div>
+											<ul>
+												<li>sass</li>
+												<li>react-animated-tree</li>
+												<li>@statickit/react</li>
+											</ul>
+										</div>
+									</div>
 								</div>
-							</div>
-							<div className="col">
-								<div>
-									<ul>
-										<li>react-icons</li>
-										<li>react-text-rotator</li>
-										<li>react-icons</li>
-									</ul>
-								</div>
-							</div>
-							<div className="col">
-								<div>
-									<ul>
-										<li>sass</li>
-										<li>react-animated-tree</li>
-										<li>@statickit/react</li>
-									</ul>
-								</div>
-							</div>
+							</Alert>
 						</div>
-                        </Alert>
-					</div>
+					</Animated>
 				</div>
+
 				<div className="col">
-					<div>
-						<Content />
-					</div>
+					<Animated
+						animationIn="bounceInRight"
+						animationOut="fadeOut"
+						animationInDuration={3000}
+						isVisible={true}
+					>
+						<div>
+							<Content />
+						</div>
+					</Animated>
 				</div>
 			</div>
 		</div>
