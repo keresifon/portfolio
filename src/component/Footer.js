41580a5f1334a345450fdf77c91e1181da73d03a
Keresifon <keresifon@gmail.com>
2020-11-25 11:51:04 +0100

    Update Footer.js

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { Navbar, Nav  } from 'react-bootstrap';
 
 
+
 function Footer(props) {
   return (
    <div  >
     <Navbar bg="dark" variant="dark"   className="footer" >
     <Nav className="mr-auto">
     <Nav.Link>This site is set up and running on AWS Amplify</Nav.Link>
+    </Nav>
+    <Nav className="ml-auto">
+    <a className= "alert" href="http://aws3.akanchawa.com"> On S3 Bucket</a> 
+
     </Nav>
     
     
